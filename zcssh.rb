86b889b72f41847f8959c67ae91e08d94ffe271d
Ber Zoidberg <ber.zoidberg@gmail.com>
2019-01-12 20:26:03 -0800

    initial import of zcssh formula

---
+++ zcssh.rb
@@ -0,0 +1,9 @@
+class Zcssh < Formula
+    desc "Fast ClusterSSH for iTerm2 (OSX)"
+    homepage "https://github.com/zoidbb/zcssh"
+    url "https://github.com/zoidbb/zcssh/archive/v0.1.0.tar.gz"
+    sha256 "8b8170a81eccfa5105f9150cbfe4d9cefda09cb653e26854b844b1f7a3197e28"
+    head "https://github.com/zoidbb/zcssh.git"
+
+    def install
+        bin.install "zcssh"
