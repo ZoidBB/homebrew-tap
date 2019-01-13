645797905813c7e341d83c4cd63ad6f7240bf6c3
Ber Zoidberg <ber.zoidberg@gmail.com>
2019-01-12 21:19:05 -0800

    bump ec2-zcssh tot 0.1.3

--- ec2-zcssh.rb
+++ ec2-zcssh.rb
@@ -2,11 +2,12 @@ class Ec2Zcssh < Formula
     include Language::Python::Virtualenv
     desc "Fast ClusterSSH for iTerm2 (OSX) - AWS Enhanced"
     homepage "https://github.com/zoidbb/ec2-zcssh"
-    url "https://github.com/zoidbb/ec2-zcssh/archive/v0.1.2.tar.gz"
-    sha256 "9b6f568d11a4aeccb6b8a898a819ce624f6b425486dfe4e2d82d091a3f8f9108"
+    url "https://github.com/zoidbb/ec2-zcssh/archive/v0.1.3.tar.gz"
+    sha256 "40782614ae333539f2f63485e20c879cc36ff7e46a3f93b0355160a1a3f97d9f"
     head "https://github.com/zoidbb/ec2-zcssh.git"
 
     depends_on "python"
+    depends_on "zcssh"
 
     def install
         venv = virtualenv_create(libexec, "python3")
